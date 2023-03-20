5d3979db11125ed6937fd48cdae6387dc826d884
angelhdzmultimedia <angel.hernandez.12@live.com>
2023-03-19 20:21:24 -0400

    1 file modified

--- src/stores/authentication/state.ts
+++ src/stores/authentication/state.ts
@@ -1,4 +1,4 @@
-import type { AuthenticatedUser } from '~~/src/types/authenticated-user'
+import type { AuthenticatedUser } from '~/types/authenticated-user'
 
 export const user = ref<AuthenticatedUser | undefined>()
 export const isAuthenticated = ref<boolean>(false)
